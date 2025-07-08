0fb9a1c0abfe2b186fea8f1e1f4a6da1ce90a6da
ADITYA_DR <raybandarkaraditya@gmail.com>
2025-07-08 20:52:51 +0530

    tes

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from "./config/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
+
+const app = express();
+const PORT = 3000;
+
+//middleware
+app.use(express.json())
+app.use(cors())
+app.use(clerkMiddleware())
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+
+await connectDB().then(()=>app.listen(PORT,()=>{
+console.log(`Server is listening on http://localhost:${PORT}`);
+
+}))
+
+app.get('/',(req,res)=>{
+    res.send("Server is live!")
+})
+
+
